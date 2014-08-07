dc7f5e414a5619abe75d915b2b4dfc8480558667
calexity <alexa@carbonfive.com>
2014-08-06 21:10:27 -0700

    layout styles

--- app/src/main/java/com/sf/funcheap/FeedActivity.java
+++ app/src/main/java/com/sf/funcheap/FeedActivity.java
@@ -1,12 +1,10 @@
 package com.sf.funcheap;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
-public class FeedActivity extends ActionBarActivity {
+public class FeedActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,22 +13,4 @@ public class FeedActivity extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.feed, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
